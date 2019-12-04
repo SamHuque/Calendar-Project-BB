eb88d7856f53a4e15bc1ae3a9e888e196e92d373
Samiul Huque <Samiul.Huque@gmail.com>
2019-12-04 13:07:25 -0500

    updated script file with code to create a ics file. A sample ics file is also pushed up with this since I ran the server and the ics file was created in the directory

---
+++ script.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import send_file
+from ics import Calendar, Event
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/get-cal")
+def get_cal():
+    c = Calendar()
+    e = Event()
+    e.name = "My cool event"
+    e.begin = '2019-11-11 00:00:00'
+    c.events.add(e)
+    with open('my.ics', 'w') as my_file:
+        my_file.writelines(c)
+    return send_file("my.ics", mimetype="text/calendar", as_attachment=True)
+
+
+# port = int(os.environ.get("PORT", 5000))
+# uncomment the above for heroku
+port = 3500
+app.run(host='0.0.0.0', port=port)
+
+# this must be run with python3 and not python
+# ex: python3 script.py
