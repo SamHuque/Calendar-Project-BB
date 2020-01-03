638006fda6804c0abf8a6fae1eaaf8c4f3525910
Samiul Huque <Samiul.Huque@gmail.com>
2020-01-03 11:27:30 -0500

    changed created1 file

--- create1.py
+++ create1.py
@@ -16,35 +16,38 @@ create_users = "CREATE TABLE users (uuid serial PRIMARY KEY, first_name varchar(
 create_events = "CREATE TABLE events (event_id serial PRIMARY KEY, event_name varchar(255), event_location varchar(255), event_type varchar(255), start_time timestamp, end_time timestamp, act integer)"
 create_users_events = "CREATE TABLE users_events (id serial PRIMARY KEY, user_id integer, event_id integer)"
 
-cur.execute(create_users)
-cur.close()
-connection.commit()
-
-cur = connection.cursor()
-cur.execute(create_events)
-cur.close()
-connection.commit()
-
-cur = connection.cursor()
-cur.execute(create_users_events)
-cur.close()
-connection.commit()
-
-print("Tables Created")
-print("Adding Data...")
-
 insert_user1 = "INSERT INTO users (uuid, first_name, last_name) VALUES (1, 'Owen', 'Outlook')"
-
 insert_user2 = "INSERT INTO users (uuid, first_name, last_name) VALUES (2, 'Glenda', 'Google')"
 
-cur = connection.cursor()
+cur.execute(create_users)
+cur.execute(create_events)
+cur.execute(create_users_events)
 cur.execute(insert_user1)
-cur.close()
-connection.commit()
-
-cur = connection.cursor()
 cur.execute(insert_user2)
 cur.close()
 connection.commit()
 
-print("Users Added")
+# cur = connection.cursor()
+# cur.execute(create_events)
+# cur.close()
+# connection.commit()
+
+# cur = connection.cursor()
+# cur.execute(create_users_events)
+# cur.close()
+# connection.commit()
+
+# print("Tables Created")
+# print("Adding Data...")
+
+# cur = connection.cursor()
+# cur.execute(insert_user1)
+# cur.close()
+# connection.commit()
+
+# cur = connection.cursor()
+# cur.execute(insert_user2)
+# cur.close()
+# connection.commit()
+
+# print("Users Added")
